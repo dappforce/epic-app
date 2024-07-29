191cd66fbe0cc4df71c65770a427989d1b586017
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-07-29 23:55:27 +0700

    Fix validation

--- src/components/chats/UnapprovedMemeCount.tsx
+++ src/components/chats/UnapprovedMemeCount.tsx
@@ -53,11 +53,12 @@ export default function UnapprovedMemeCount({
     return <Skeleton className={cx('my-0.5', className)} />
 
   const blockedFactor = (blocked / (approved + blocked)) * 100
+  const approvedOrBlockedCount = approved + blocked
 
   return (
     <div className={cx('rounded-full text-sm text-text-muted', className)}>
       <div className='flex flex-shrink-0 items-center gap-2'>
-        {(data?.length ?? 0) >= 5 && blockedFactor > 0 && (
+        {approvedOrBlockedCount >= 5 && blockedFactor > 0 && (
           <div className='mr-1 flex flex-shrink-0 items-center gap-1 rounded-full border-2 border-yellow-600 px-2 py-0.5 text-yellow-500'>
             💩 {blockedFactor.toFixed(0)}%
           </div>
