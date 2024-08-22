6fc18c80c23e767f5e576bc1e866b907ac9f8409
Sam <sam4uk.v1ad@gmail.com>
2024-08-22 16:07:23 +0300

    Update after merge

--- src/services/datahub/generated-query.ts
+++ src/services/datahub/generated-query.ts
@@ -704,7 +704,6 @@ export type IdTimestampPair = {
 export enum IdentityProvider {
   Email = 'EMAIL',
   Evm = 'EVM',
-  Solana = 'SOLANA',
   Facebook = 'FACEBOOK',
   Farcaster = 'FARCASTER',
   Google = 'GOOGLE',
