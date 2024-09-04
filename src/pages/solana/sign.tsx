2b59e251a930b9de4818da31b38eebfbac406e81
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-09-04 22:05:24 +0700

    Add sign message button

--- src/pages/solana/sign.tsx
+++ src/pages/solana/sign.tsx
@@ -28,89 +28,93 @@ async function getLinkIdentitySolanaMessage(address: string) {
   return res?.linkedIdentityExternalProviderSolanaProofMsg?.message
 }
 
-export const getServerSideProps = getCommonServerSideProps(
-  {},
-  async (context) => {
-    const signer = context.query.signer as string
-    const signerNonce = context.query.signer_nonce as string
-    const address = context.query.address as string
-    const phantomEncryptionPublicKey = context.query
-      .phantom_encryption_public_key as string
-    const data = context.query.data as string
-    const nonce = context.query.nonce as string
+export const getServerSideProps = getCommonServerSideProps<{
+  link?: string
+  error?: string
+}>({}, async (context) => {
+  const signer = context.query.signer as string
+  const signerNonce = context.query.signer_nonce as string
+  const address = context.query.address as string
+  const phantomEncryptionPublicKey = context.query
+    .phantom_encryption_public_key as string
+  const data = context.query.data as string
+  const nonce = context.query.nonce as string
 
-    const errorMessage = context.query.errorMessage as string
-    if (errorMessage) {
-      return {
-        props: {
-          error: errorMessage,
-        },
-      }
-    }
-
-    if (
-      !signer ||
-      !signerNonce ||
-      !address ||
-      !phantomEncryptionPublicKey ||
-      !data ||
-      !nonce
-    ) {
-      return {
-        props: {
-          error: 'Missing required parameters',
-        },
-      }
-    }
-
-    const sharedSecretDapp = nacl.box.before(
-      bs58.decode(phantomEncryptionPublicKey),
-      bs58.decode(env.DAPP_SECRET_KEY)
-    )
-    const connectData = decryptPayload(data, nonce, sharedSecretDapp) as {
-      public_key: string
-      session: string
-    }
-
-    const decrypted = nacl.secretbox.open(
-      Buffer.from(signer, 'hex') as unknown as Uint8Array,
-      bs58.decode(signerNonce),
-      bs58.decode(env.DAPP_SECRET_KEY)
-    )
-    if (!decrypted) {
-      return {
-        props: {
-          error: 'Bad Request',
-        },
-      }
-    }
-
-    const message = await getLinkIdentitySolanaMessage(connectData.public_key)
-    const [signNonce, encrypted] = encryptPayload(
-      {
-        message: bs58.encode(Buffer.from(message) as any),
-        session: connectData.session,
-      },
-      sharedSecretDapp
-    )
-
-    const params = {
-      dapp_encryption_public_key: env.NEXT_PUBLIC_DAPP_PUBLIC_KEY,
-      nonce: bs58.encode(signNonce),
-      payload: bs58.encode(encrypted),
-      redirect_link: `${env.NEXT_PUBLIC_BASE_URL}/solana/connect?solana_address=${connectData.public_key}&signer=${signer}&signer_nonce=${signerNonce}&address=${address}&message=${message}&phantom_encryption_public_key=${phantomEncryptionPublicKey}`,
-    }
-
+  const errorMessage = context.query.errorMessage as string
+  if (errorMessage) {
     return {
-      redirect: {
-        destination: `https://phantom.app/ul/v1/signMessage?${new URLSearchParams(
-          params
-        ).toString()}`,
-        permanent: false,
+      props: {
+        error: errorMessage,
       },
     }
   }
-)
+
+  if (
+    !signer ||
+    !signerNonce ||
+    !address ||
+    !phantomEncryptionPublicKey ||
+    !data ||
+    !nonce
+  ) {
+    return {
+      props: {
+        error: 'Missing required parameters',
+      },
+    }
+  }
+
+  const sharedSecretDapp = nacl.box.before(
+    bs58.decode(phantomEncryptionPublicKey),
+    bs58.decode(env.DAPP_SECRET_KEY)
+  )
+  const connectData = decryptPayload(data, nonce, sharedSecretDapp) as {
+    public_key: string
+    session: string
+  }
+
+  const decrypted = nacl.secretbox.open(
+    Buffer.from(signer, 'hex') as unknown as Uint8Array,
+    bs58.decode(signerNonce),
+    bs58.decode(env.DAPP_SECRET_KEY)
+  )
+  if (!decrypted) {
+    return {
+      props: {
+        error: 'Bad Request',
+      },
+    }
+  }
+
+  const message = await getLinkIdentitySolanaMessage(connectData.public_key)
+  const [signNonce, encrypted] = encryptPayload(
+    {
+      message: bs58.encode(Buffer.from(message) as any),
+      session: connectData.session,
+    },
+    sharedSecretDapp
+  )
+
+  const params = {
+    dapp_encryption_public_key: env.NEXT_PUBLIC_DAPP_PUBLIC_KEY,
+    nonce: bs58.encode(signNonce),
+    payload: bs58.encode(encrypted),
+    redirect_link: `${env.NEXT_PUBLIC_BASE_URL}/solana/connect?solana_address=${connectData.public_key}&signer=${signer}&signer_nonce=${signerNonce}&address=${address}&message=${message}&phantom_encryption_public_key=${phantomEncryptionPublicKey}`,
+  }
+
+  return {
+    props: {
+      address,
+      solanaAddress: connectData.public_key,
+      encrypted: bs58.encode(encrypted),
+      nonce: bs58.encode(signNonce),
+      signer,
+      phantomEncryptionPublicKey,
+      signerNonce,
+      message,
+    },
+  }
+})
 
 export default function SolanaSignPage(
   props:
